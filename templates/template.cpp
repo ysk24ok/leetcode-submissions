80e886f0cd120602cf95b54c54c7fb7eb353da5a
Yusuke NISHIOKA <yusuke.nishioka.0713@gmail.com>
2019-10-28 21:27:41 +0900

    Add a template for C++

---
+++ templates/template.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+
+};
+
+int main() {
+  Solution sol;
+}

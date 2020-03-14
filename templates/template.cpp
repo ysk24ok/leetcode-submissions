2e21aac9f6322c481d51bf1e364b19cb6f98e2b1
Yusuke NISHIOKA <yusuke.nishioka.0713@gmail.com>
2020-03-15 00:27:56 +0900

    Update template.cpp

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,12 +1,15 @@
 #include <algorithm>
 #include <cassert>
 #include <iostream>
+#include <iterator>
 #include <limits>
 #include <numeric>
 #include <queue>
 #include <set>
+#include <stack>
 #include <string>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 using namespace std;

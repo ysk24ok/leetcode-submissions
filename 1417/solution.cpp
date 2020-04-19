cf3830e7fb1a0eb16972618885d6f7140fc1ec37
Yusuke NISHIOKA <yusuke.nishioka.0713@gmail.com>
2020-04-19 16:33:02 +0900

    Add solutions for Weekly Contest 185

---
+++ 1417/solution.cpp
@@ -0,0 +1,68 @@
+#include <cctype>
+#include <queue>
+#include <string>
+
+#include "leetcode/assert.hpp"
+
+using namespace std;
+using namespace LeetCode;
+
+class Solution {
+ public:
+  string reformat(string s) {
+    queue<char> number_q;
+    queue<char> alpha_q;
+    for (const auto& c : s) {
+      // number
+      if (isdigit(static_cast<int>(c)) != 0) {
+        number_q.push(c);
+      } else {
+        alpha_q.push(c);
+      }
+    }
+    string ret;
+    int diff = number_q.size() - alpha_q.size();
+    if (diff == 1 || diff == 0) { push(ret, number_q, alpha_q); }
+    else if (diff == -1) { push(ret, alpha_q, number_q); }
+    else { return ret; }
+    return ret;
+  }
+
+  void push(string& ret, queue<char>& q1, queue<char>& q2) {
+    assert(q1.size() >= q2.size());
+    while (!q1.empty() || !q2.empty()) {
+      char c1 = q1.front();
+      q1.pop();
+      ret += c1;
+      if (q2.empty()) continue;
+      char c2 = q2.front();
+      q2.pop();
+      ret += c2;
+    }
+  }
+};
+
+int main() {
+  Solution sol;
+  string s, expected;
+
+  s = "a0b1c2";
+  expected = "0a1b2c";
+  ExpectEqual(expected, sol.reformat(s));
+
+  s = "leetcode";
+  expected = "";
+  ExpectEqual(expected, sol.reformat(s));
+
+  s = "1229857369";
+  expected = "";
+  ExpectEqual(expected, sol.reformat(s));
+
+  s = "covid2019";
+  expected = "c2o0v1i9d";
+  ExpectEqual(expected, sol.reformat(s));
+
+  s = "ab123";
+  expected = "1a2b3";
+  ExpectEqual(expected, sol.reformat(s));
+}
--- leetcode/assert.hpp
+++ leetcode/assert.hpp
@@ -6,13 +6,33 @@
 namespace LeetCode {
 
 template <typename T>
-std::string Vector2String(const std::vector<T> vec) {
+std::string Vector2String(const std::vector<T>& vec) {
   std::ostringstream oss;
+  size_t m = vec.size();
+  if (m == 0) { return "[]"; }
   oss << "[";
-  for (size_t i = 0; i < vec.size() - 1; ++i) {
+  for (size_t i = 0; i < m - 1; ++i) {
     oss << vec[i] << ",";
   }
-  oss << vec[vec.size() - 1] << "]";
+  oss << vec[m - 1] << "]";
+  return oss.str();
+}
+
+template<typename T>
+std::string Matrix2String(const std::vector<std::vector<T>>& mat) {
+  std::ostringstream oss;
+  size_t m = mat.size();
+  if (m == 0) { return "[]"; }
+  size_t n = mat[0].size();
+  oss << "[";
+  for (size_t i = 0; i < m - 1; ++i) {
+    oss << "[";
+    for (size_t j = 0; j < n - 1; ++j) {
+      oss << mat[i][j] << ",";
+    }
+    oss << mat[i][n - 1] << "]";
+  }
+  oss << "]";
   return oss.str();
 }
 
@@ -62,6 +82,28 @@ bool ExpectVectorUnorderedEqual(const std::vector<T>& expected,
   if (!ret) {
     std::cerr << "ExpectVectorUnorderedEqual failed, expected: " << Vector2String(expected)
               << ", actual: " << Vector2String(actual) << std::endl;
+  }
+  return ret;
+}
+
+template <typename T>
+bool ExpectMatrixEqual(const std::vector<std::vector<T>>& expected,
+                         const std::vector<std::vector<T>>& actual) {
+  if (expected.size() != actual.size()) {
+    std::cerr << "ExpectMatrixEqual failed, expected size: " << expected.size()
+              << ", actual size: " << actual.size()
+              << ", actual vector: " << Matrix2String(actual) << std::endl;
+    return false;
+  }
+  bool ret = true;
+  for (size_t i = 0; i < expected.size(); ++i) {
+    for (size_t j = 0; j < expected[i].size(); ++j) {
+      if (expected[i][j] != actual[i][j]) { ret = false; }
+    }
+  }
+  if (!ret) {
+    std::cerr << "ExpectMatrixEqual failed, expected: " << Matrix2String(expected)
+              << ", actual: " << Matrix2String(actual) << std::endl;
   }
   return ret;
 }

23dee0c7631d0f43cad266a58e80d71b5d630669
Yusuke NISHIOKA <yusuke.nishioka.0713@gmail.com>
2020-07-05 17:06:24 +0900

    Add a solution for 0461

---
+++ 0461/solution.py
@@ -0,0 +1,17 @@
+from typing import *
+
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        diff = 0
+        for _ in range(32):
+            if x % 2 != y % 2:
+                diff += 1
+            x >>= 1
+            y >>= 1
+        return diff
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    assert sol.hammingDistance(1, 4) == 2

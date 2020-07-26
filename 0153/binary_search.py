9e3202177e3484c134db65b9abdb0e26961b687f
Yusuke NISHIOKA <yusuke.nishioka.0713@gmail.com>
2020-07-26 09:14:38 +0900

    Add an another solution for 153

---
+++ 0153/binary_search.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left + 1 < right:
+            mid = int((right - left) / 2) + left
+            # pivot in the left half
+            if nums[left] > nums[mid]:
+                right = mid
+            # pivot is in the right half
+            elif nums[mid] > nums[right]:
+                left = mid
+            # no pivot is in this search space
+            else:
+                break
+        return min(nums[left], nums[right])
+
+
+if __name__ == '__main__':
+    sol = Solution()
+
+    nums, expected = [3, 4, 5, 1, 2], 1
+    assert sol.findMin(nums) == expected
+
+    nums, expected = [4, 5, 6, 7, 0, 1, 2], 0
+    assert sol.findMin(nums) == expected
+
+    nums, expected = [1, 2, 3], 1
+    assert sol.findMin(nums) == expected

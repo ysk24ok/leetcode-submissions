29f9fccf9aec18d0f70b4cd12305ffb30280cb40
Yusuke NISHIOKA <yusuke.nishioka.0713@gmail.com>
2020-11-22 11:18:48 +0900

    Add constuctors of LeetCode::ListNode

--- leetcode/linked_list.hpp
+++ leetcode/linked_list.hpp
@@ -5,7 +5,9 @@ namespace LeetCode {
 struct ListNode {
   int val;
   ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 ListNode* CreateListNode(const std::vector<int>& vec);
